5880b1b764a8756cedfe5ed28d408cdbb751200d
Omar Crosby <omar.crosby@gmail.com>
2023-10-15 19:17:52 -0400

    chore: remove defunct comment

--- src/App.js
+++ src/App.js
@@ -34,7 +34,6 @@ function App() {
     return (
     <div className="App">
         <h2>Let's get started!</h2>
-        <p>This is also visible!</p>
         <Expenses items={expenses} />
     </div>
   );
