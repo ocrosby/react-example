d717e222adf10e2fb82bf2925c727e5e9d9e1ac5
Omar Crosby <omar.crosby@gmail.com>
2023-10-29 18:50:12 -0400

    chore: using state

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,19 +1,31 @@
+import React, {useState} from 'react';
+
 import './ExpenseItem.css';
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
+
 function ExpenseItem(props) {
-    return (
-        <Card className="expense-item">
-            <ExpenseDate
-                date={props.date}
-            />
-            <div className="expense-item__description">
-                <h2>{props.title}</h2>
-                <div className="expense-item_price">${props.amount}</div>
-            </div>
-        </Card>
-    );
+  const [title, setTitle] = useState(props.title);
+  console.log('ExpenseItem evaluated by React');
+
+  const clickHandler = () => {
+      setTitle('Updated!');
+      console.log(title);
+  }
+
+  return (
+      <Card className="expense-item">
+          <ExpenseDate
+              date={props.date}
+          />
+          <div className="expense-item__description">
+              <h2>{title}</h2>
+              <div className="expense-item_price">${props.amount}</div>
+          </div>
+          <button onClick={clickHandler}>Change Title</button>
+      </Card>
+  );
 }
 
 export default ExpenseItem;
